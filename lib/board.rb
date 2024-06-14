224de647499c2e09303af19362330c6f915b25f1
ElectronDragon <electricdragondevelopment@gmail.com>
2024-06-14 08:36:19 +0100

    Create board.rb and require in main

---
+++ lib/board.rb
@@ -0,0 +1,14 @@
+class Board
+    def initialize()
+        @A1 = " "
+        @A2 = " "
+        @A3 = " "
+        @B1 = " "
+        @B2 = " "
+        @B3 = " "
+        @C1 = " "
+        @C2 = " "
+        @C3 = " "
+        puts "The board is ready!"
+    end
+end
--- main.rb
+++ main.rb
@@ -1,4 +1,11 @@
+require_relative 'lib/board'
+
 # Need to create a blank board and display it
     # Initial thought is printing series of arrays
+    # Better yet, data stored in array, but displayed in string containing index variables!!!
 # Need two players, and a series of prompts asking for their moves, which update the arrays
 # Need logic to determine when the game has been won
+
+new_board = Board.new
+
+p new_board
