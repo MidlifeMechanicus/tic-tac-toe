a00c4179c02f1a17c73088ea127f51158ea9a46e
ElectronDragon <electricdragondevelopment@gmail.com>
2024-06-14 08:03:32 +0100

    Rename script.rb to main.rb and create lib folder, then  add psueudo code notes to main.rb

---
+++ main.rb
@@ -0,0 +1,4 @@
+# Need to create a blank board and display it
+    # Initial thought is printing series of arrays
+# Need two players, and a series of prompts asking for their moves, which update the arrays
+# Need logic to determine when the game has been won
