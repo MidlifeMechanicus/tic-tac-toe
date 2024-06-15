421f84486a86ce0aeda36b4a87ee7ee0fb6cfa61
ElectronDragon <electricdragondevelopment@gmail.com>
2024-06-15 07:21:12 +0100

    Add valid_choice and WHILE loop to get_move so players don't miss turns through accidental entries

--- lib/board/get_move.rb
+++ lib/board/get_move.rb
@@ -2,32 +2,45 @@ module GetMove
     def get_move
         self.turn % 2 == 0 ? mark = "O" : mark = "X"
         self.turn % 2 == 0 ? player = "Player 2" : player = "Player 1"
+        valid_choice = false
         # colorise?
         # Similar if statement for player handles
         # Need move_valid variable for while loop
-        puts "What grid square would you like to claim, #{player}?"
-        move = gets.chomp.to_i
-        if move == 1 && self.A1 == 1
-            self.A1 = mark
-        elsif move == 2 && self.A2 == 2
-            self.A2 = mark
-        elsif move == 3 && self.A3 == 3
-            self.A3 = mark
-        elsif move == 4 && self.B1 == 4
-            self.B1 = mark
-        elsif move == 5 && self.B2 == 5
-            self.B2 = mark
-        elsif move == 6 && self.B3 == 6
-            self.B3 = mark
-        elsif move == 7 && self.C1 == 7
-            self.C1 = mark
-        elsif move == 8 && self.C2 == 8
-            self.C2 = mark
-        elsif move == 9 && self.C3 == 9
-            self.C3 = mark
-        else
-            puts "That is not a valid option."
+        while valid_choice == false
+          puts "What grid square would you like to claim, #{player}?"
+          move = gets.chomp.to_i
+          if move == 1 && self.A1 == 1
+              self.A1 = mark
+              valid_choice = true
+          elsif move == 2 && self.A2 == 2
+              self.A2 = mark
+              valid_choice = true
+          elsif move == 3 && self.A3 == 3
+              self.A3 = mark
+              valid_choice = true
+          elsif move == 4 && self.B1 == 4
+              self.B1 = mark
+              valid_choice = true
+          elsif move == 5 && self.B2 == 5
+              self.B2 = mark
+              valid_choice = true
+          elsif move == 6 && self.B3 == 6
+              self.B3 = mark
+              valid_choice = true
+          elsif move == 7 && self.C1 == 7
+              self.C1 = mark
+              valid_choice = true
+          elsif move == 8 && self.C2 == 8
+              self.C2 = mark
+              valid_choice = true
+          elsif move == 9 && self.C3 == 9
+              self.C3 = mark
+              valid_choice = true
+          else
+              puts "That is not a valid option."
+          end
         end
         self.turn +=1
+        valid_choice = false
     end
 end
